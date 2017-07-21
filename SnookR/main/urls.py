a046cb953e48f19488d184f280b08837d8f6ed50
Bobby Eshleman <eshleman@pdx.edu>
2017-07-21 10:05:47 -0700

    Created login, signup, and base templates and views

--- SnookR/main/urls.py
+++ SnookR/main/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from main import views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+#    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^home/$', views.HomeView.as_view(), name='home'),
 ]
