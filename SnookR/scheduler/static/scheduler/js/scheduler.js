1f7e9002a4617ec4cfb83778128cc1561666ff92
Bobby Eshleman <eshleman@pdx.edu>
2017-12-05 22:43:14 -0800

    Put getSessionList api call in scehduler.js

--- SnookR/scheduler/static/scheduler/js/scheduler.js
+++ SnookR/scheduler/static/scheduler/js/scheduler.js
@@ -1,89 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let division = $('#id_division').val();
     console.log('divison=', division);
 
     api.getSessionList()
-        .done(function(data) {
-            console.log('sessions=', data);
-        });
-
-
-
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay,listWeek'
-        },
-        defaultDate: '2017-11-12',
-        navLinks: true, // can click day/week names to navigate views
-        editable: false,
-        eventLimit: true, // allow "more" link when too many events
-        eventClick: function(calEvent, jsEvent, view) {
-            console.log('eventClick', calEvent);
-
-
-            // Expand the right column
-            $('#right-column').text(calEvent.title);
-
-            // change the border color just for fun
-            $(this).css('border-color', 'red');
-        },
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2017-11-01',
-            },
-            {
-                title: 'Long Event',
-                start: '2017-11-07',
-                end: '2017-11-10'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2017-11-09T16:00:00'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2017-11-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2017-11-11',
-                end: '2017-11-13'
-            },
-            {
-                title: 'Meeting',
-                start: '2017-11-12T10:30:00',
-                end: '2017-11-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2017-11-12T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2017-11-12T14:30:00'
-            },
-            {
-                title: 'Happy Hour',
-                start: '2017-11-12T17:30:00'
-            },
-            {
-                title: 'Dinner',
-                start: '2017-11-12T20:00:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2017-11-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2017-11-28'
+        .done(function (data) {
+            let sessionList = [];
+            for (elem in data) {
+                if (data.hasOwnProperty(elem)) {
+                    sessionList.push(data[elem])
+                }
             }
-        ]
-    });
+
+            let eventList = sessionList.map(elem => {
+                return {title: elem.name, start: elem.start_date, end: elem.end_date}
+            });
+
+
+            $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay,listWeek'
+                },
+                defaultDate: '2017-11-12',
+                navLinks: true, // can click day/week names to navigate views
+                editable: false,
+                eventLimit: true, // allow "more" link when too many events
+                eventClick: function (calEvent, jsEvent, view) {
+                    console.log('eventClick', calEvent);
+
+
+                    // Expand the right column
+                    $('#right-column').text(calEvent.title);
+
+                    // change the border color just for fun
+                    $(this).css('border-color', 'red');
+                },
+                events: eventList,
+            });
+        });
 });
