9d340917cfc62c6999fa033309aa4a08667ae2ce
Bobby Eshleman <eshleman@pdx.edu>
2017-12-22 16:41:37 -0800

    Removed snooker_extras

--- SnookR/main/templatetags/snooker_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from main.models import Session, Sub
-
-register = template.Library()
-
-
-@register.simple_tag
-def get_unregister_url(session: Session, sub: Sub):
-    return session.get_unregister_url(sub)
